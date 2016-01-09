9ab4766ea42b8c9d5fac7d5ca0b2d5fdad165dd5
Rakesh Tembhurne <tj2point0@gmail.com>
2016-01-09 20:37:15 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,193 @@
+<!doctype html>
+<!--[if lt IE 7 ]><html lang="en" class="no-js ie6"> <![endif]-->
+<!--[if IE 7 ]><html lang="en" class="no-js ie7"> <![endif]-->
+<!--[if IE 8 ]><html lang="en" class="no-js ie8"> <![endif]-->
+<!--[if IE 9 ]><html lang="en" class="no-js ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--><html lang="en" class="no-js"> <!--<![endif]-->
+<head>
+<meta charset="utf-8">
+<meta name="keywords" content="licamac films, films nagpur, corporate films nagpur">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<title>Licamac Films, Nagpur</title>
+
+<!-- main JS libs -->
+<script>
+ (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+ })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ ga('create', 'UA-46079631-1', 'licamacfilms.com');
+ ga('send', 'pageview');
+
+</script>
+<!-- Style CSS -->
+<link href="css/bootstrap.css" media="screen" rel="stylesheet">
+<link href="style.css" media="screen" rel="stylesheet">
+<!-- scripts -->
+
+</head>
+
+<body>
+<div class="body_wrap">
+    <div class="container">
+
+        <!-- content -->
+        <div class="content " role="main">
+
+            <!-- row -->
+            <div class="row">
+                <div class="col-sm-12">
+                    <h1 class="foo pull-right"><img src="images/logo.png" width="200" /></h1>                    
+                    <!--/ search widget -->
+                </div>
+
+            </div>
+            <!--/ row -->
+
+            <div class="row">
+                <div class="col-sm-12">
+                    <!-- Tabs -->
+                    <div class="tabs_framed">
+                        <div class="inner">
+                            <ul class="tabs clearfix">
+                                <li class="active"><a href="#home" data-toggle="tab" hidefocus="true" style="outline: none;" class="gradient">Home</a></li>
+                                <li class=""><a href="#works" data-toggle="tab" hidefocus="true" style="outline: none;" class="gradient">Our Productions</a></li>
+                                <!--<li class=""><a href="#testimonials" data-toggle="tab" hidefocus="true" style="outline: none;" class="gradient">Testimonials</a></li>-->
+                                <li class=""><a href="#contact" data-toggle="tab" hidefocus="true" style="outline: none;" class="gradient">Contact</a></li>
+                            </ul>
+
+                            <div class="tab-content clearfix">
+                                <div class="tab-pane fade gradient active in" id="home">
+                                    <h4>Who we are</h4>
+                                    <p>Licamac Films is one of the finest production houses based in Central India creating and developing content across fiction and non- fiction genres. The team compromises of best in-house creative and technical artists at work with backgrounds emerging from national news channels to premier film institute in India.</p>
+                                    <div class="divider dashed"></div>
+                                    <h4>What we do</h4>
+                                    <p>In the advent of digital age, cinema and TV form part and parcel of urban life culture. While traditional media like print, radio and outdoor advertising retain its fair share, the audio visual medium has grown at profound pace. The medium has given birth to new avenues of audio visual story telling process like short films, ads, documentaries, corporate films. We belong to this space catering to a diversified profile spread across industries..</p>
+                                </div>
+                                <?php 
+                                    $films = array(
+                                        array(
+                                            "//www.youtube.com/embed/StZhDC86Co4", // LicaDemoReal
+                                            "//www.youtube.com/embed/wAXx8VlVNP0", // BMA
+                                            "//www.youtube.com/embed/eD2UrQv5NGI", // SBI Wagh
+                                        ),
+                                        array(
+                                            "//www.youtube.com/embed/eDZ_4D5ud0A", // KNMP
+                                            "//www.youtube.com/embed/wwUA6D_9rv0", // Solar fencing
+                                            "//www.youtube.com/embed/BuVo_iUR9Mo", // Cheque of death
+                                        ),
+                                        array(
+                                            "//www.youtube.com/embed/brfkB6JPX_Q", // Clock tower
+                                            "//www.youtube.com/embed/i51ov6gCEHI", // Baba Amte
+                                            "//www.youtube.com/embed/v3QGJSW670A", // A1 Chips
+                                        ),
+                                        array(
+                                            "//www.youtube.com/embed/gXtVBRx-v9c", // Niyati
+                                            //"//www.youtube.com/embed/qtSvcsn7X2Q", // SAM Profile
+                                            //"//www.youtube.com/embed/PFpMxU-n2fs", // Bharat camp
+                                        )
+                                    );
+                                 ?>
+                                <div class="tab-pane fade gradient" id="works">
+                                <?php foreach ($films as $videos): ?>
+                                    <div class="row">
+                                    <?php foreach ($videos as $video): ?>
+                                        <div class="col-sm-4">
+                                            <div class="video-container">
+                                                <iframe width="260" height="200" src="<?php echo $video; ?>?rel=0&showinfo=0" frameborder="0" allowfullscreen=""></iframe></div>
+                                        </div>
+                                    <?php endforeach ?>
+                                    </div>
+                                    <br />
+                                <?php endforeach ?>
+                                </div>
+                                <div class="tab-pane fade gradient" id="testimonials">
+                                    <h4>What our customers say about us</h4>
+                                    <div class="comment-list clearfix hide" id="comments">
+                                        <ol>
+                                            <li class="">
+                                                <div class="comment-body">
+                                                    <div class="inner gradient">
+                                                        <div class="comment-arrow"></div>                      
+                                                        <div class="comment-text">
+                                                            <div class="comment-author clearfix">
+                                                                <a href="#" class="link-author" hidefocus="true" style="outline: none;">Justin Timberlake</a> <span class="comment-date">CEO, Mocrosoft</span>
+                                                            </div>
+                                                            <div class="comment-entry">
+                                                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, eaque consequuntur nulla molestiae. Repellendus amet provident nisi. Nesciunt, earum, atque consequatur ducimus iste laborum nulla assumenda molestiae commodi pariatur. Dolor.
+                                                            </div>
+                                                        </div>
+                                                        <div class="clear"></div>
+                                                    </div>
+                                                </div>
+                                            </li>
+                                            <li class="">
+                                                <div class="comment-body">
+                                                    <div class="inner gradient">
+                                                        <div class="comment-arrow"></div>
+                                                        <div class="comment-text">
+                                                            <div class="comment-author clearfix">
+                                                                <a href="#" class="link-author" hidefocus="true" style="outline: none;">Larry Page</a> <span class="comment-date">Founder, Google</span>
+                                                            </div>
+                                                            <div class="comment-entry">
+                                                               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, eaque consequuntur nulla molestiae. Repellendus amet provident nisi. Nesciunt, earum, atque consequatur ducimus iste laborum nulla assumenda molestiae commodi pariatur. Dolor.
+                                                            </div>
+                                                        </div>
+                                                        <div class="clear"></div>
+                                                    </div>
+                                                </div>
+                                            </li>
+                                        </ol>
+                                    </div>
+                                </div>
+                                <div class="tab-pane fade gradient" id="contact">
+                                    <div class="row">
+                                        
+                                        <div class="col-sm-12">
+<address>
+                                            
+                                            <h4>Sameer Dandekar</h4>
+                                            Mobile: 9326281773 <br><br>
+                                            Email: <a href="mailto:licamacfilm@gmail.com">licamacfilm@gmail.com</a><br><br><br><br>
+                                            
+                                            <h4>Rutu Borkar</h4>
+                                            Mobile: 9373625666 <br><br>
+                                            Email: <a href="mailto:licamac.thepeople@gmail.com">licamac.thepeople@gmail.com</a><br><br><br><br>
+                                            
+
+                                              <h4>Licamac Films</h4>
+                                              
+                                              
+                                              Plot no. 1341, Parvati Nagar,<br><br>
+                                              Near Ring Road, Nagpur 440 027<br><br>
+                                              Phone: (0712) 2780530<br><br>
+                                              
+                                            </address>
+                                            <br /><br><br>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="bottom"></div>
+                    </div>
+                    <!--/ Tabs -->
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-sm-12 center">
+                    <small class="">&copy; 2013-2014 Licamac Films</small>
+                </div>
+            </div>
+
+        </div>
+        <!--/ content -->
+    </div>
+    <!--/ container -->
+</div>
+<script src="js/libs/jquery-1.10.0.js"></script>
+<script src="js/libs/bootstrap.min.js"></script>
+</body>
+</html>
